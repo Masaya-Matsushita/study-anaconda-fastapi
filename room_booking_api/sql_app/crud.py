4b3c15d668cbc84dc389fcbe6366b83ff6d38cd2
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-12 16:53:52 +0900

    ✨ 会議室予約アプリを作成

--- room_booking_api/sql_app/crud.py
+++ room_booking_api/sql_app/crud.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from . import models, schemas
 
@@ -14,7 +15,7 @@ def get_bookings(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Booking).offset(skip).limit(limit).all()
 
 # ユーザー登録
-def create_user(db: Session, user: schemas.User):
+def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(username=user.username)
     db.add(db_user)
     db.commit()
@@ -22,7 +23,7 @@ def create_user(db: Session, user: schemas.User):
     return db_user
 
 # 会議室登録
-def create_room(db: Session, room: schemas.Room):
+def create_room(db: Session, room: schemas.RoomCreate):
     db_room = models.Room(room_name=room.room_name, capacity=room.capacity)
     db.add(db_room)
     db.commit()
@@ -30,15 +31,23 @@ def create_room(db: Session, room: schemas.Room):
     return db_room
 
 # 予約登録
-def create_booking(db: Session, booking: schemas.Booking):
-    db_booking = models.Booking(
-        user_id = booking.user_id,
-        room_id = booking.room_id,
-        booked_num = booking.booked_num,
-        start_datetime = booking.start_datetime,
-        end_datetime = booking.end_datetime
-    )
-    db.add(db_booking)
-    db.commit()
-    db.refresh(db_booking)
-    return db_booking
+def create_booking(db: Session, booking: schemas.BookingCreate):
+    db_booked = db.query(models.Booking).\
+        filter(models.Booking.room_id == booking.room_id).\
+        filter(models.Booking.end_datetime > booking.start_datetime).\
+        filter(models.Booking.start_datetime < booking.end_datetime).\
+        all()
+    if len(db_booked) == 0:
+        db_booking = models.Booking(
+            user_id = booking.user_id,
+            room_id = booking.room_id,
+            booked_num = booking.booked_num,
+            start_datetime = booking.start_datetime,
+            end_datetime = booking.end_datetime
+        )
+        db.add(db_booking)
+        db.commit()
+        db.refresh(db_booking)
+        return db_booking
+    else:
+        raise HTTPException(status_code=404, detail="Already booked")
